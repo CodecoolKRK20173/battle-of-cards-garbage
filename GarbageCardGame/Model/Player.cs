825232167805512f8bf05a11c205edf39fa8bc5e
Tomasz Giela <chrono5@wp.pl>
2020-03-18 09:20:04 +0100

    Add Player class implementation

--- GarbageCardGame/Model/Player.cs
+++ GarbageCardGame/Model/Player.cs
@@ -6,5 +6,12 @@ namespace GarbageCardGame.Model
 {
     class Player
     {
+        public string Name { get; private set; }
+        public Hand Hand { get; set; }
+
+        public void PickStat()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
