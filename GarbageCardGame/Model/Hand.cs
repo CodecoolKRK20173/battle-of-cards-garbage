472f5fedf96a4b18bbb4469797692d559618ef97
Tomasz Giela <chrono5@wp.pl>
2020-03-18 09:45:20 +0100

    Add Hand class implementation

--- GarbageCardGame/Model/Hand.cs
+++ GarbageCardGame/Model/Hand.cs
@@ -6,5 +6,26 @@ namespace GarbageCardGame.Model
 {
     class Hand
     {
+        public List<Card> CardsInHand { get; private set; }
+
+        public Hand()
+        {
+            CardsInHand = GetHand();
+        }
+
+        private List<Card> GetHand()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddCard(Card card)
+        {
+            CardsInHand.Add(card);
+        }
+
+        public void RemoveCard(Card card)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
