9d964d69369ee767a115f310533c0d1a12feeced
Tomasz Giela <chrono5@wp.pl>
2020-03-19 16:35:22 +0100

    Add testing version for xml card deck serialization

--- GarbageCardGame/MainApp.cs
+++ GarbageCardGame/MainApp.cs
@@ -1,5 +1,10 @@
 ﻿using GarbageCardGame.View;
 using GarbageCardGame.Controller;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using GarbageCardGame.Model;
+using System.IO;
+using System;
 
 namespace GarbageCardGame
 {
@@ -9,7 +14,9 @@ namespace GarbageCardGame
         {
             ViewGarbage View = new ViewGarbage();
             GameController Game = new GameController();
-            
+
+            SerializeToXml(Game.GameDeck.CardDeck, Environment.CurrentDirectory + @"..\..\..\..\Resorces\waste.xml");
+
             View.Print("Welcome to the game.\nThe cards are already dealt.\nIt is Player1 turn...");
             System.Threading.Thread.Sleep(3000);
             while (!Game.AnyPlayerHasWon())
@@ -17,6 +24,25 @@ namespace GarbageCardGame
                 Game.PlayRound(Game.Player1, Game.Player2);
             }
             Game.EndGameScenario();
+        }
+
+        public static void SerializeToXml(List<Card> cards, string filePath)
+        {
+            XmlSerializer XMLSerializer = new XmlSerializer(typeof(List<Card>));
+            TextWriter textWriter = new StreamWriter(filePath);
+            XMLSerializer.Serialize(textWriter, cards);
+            textWriter.Close();
+        }
+
+        public static List<Card> DeserializeFromXml(string filePath)
+        {
+            XmlSerializer deserializer = new XmlSerializer(typeof(List<Card>));
+            TextReader tr = new StreamReader(@filePath);
+            List<Card> movie;
+            movie = (List<Card>)deserializer.Deserialize(tr);
+            tr.Close();
+
+            return movie;
         }
     }
 }
