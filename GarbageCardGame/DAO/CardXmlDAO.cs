8ba843cf23d877dd2f707bc5f7ca5a3279e71b14
Tomasz Giela <chrono5@wp.pl>
2020-03-21 10:46:44 +0100

    Add CardXmlDAO class

---
+++ GarbageCardGame/DAO/CardXmlDAO.cs
@@ -0,0 +1,37 @@
+﻿using GarbageCardGame.Model;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace GarbageCardGame.DAO
+{
+    class CardXmlDAO : ICardDAO
+    {
+        public List<Card> GetDeck()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SerializeToXml(List<Card> cards, string filePath)
+        {
+            XmlSerializer XMLSerializer = new XmlSerializer(typeof(List<Card>));
+            using (TextWriter textWriter = new StreamWriter(filePath))
+            {
+                XMLSerializer.Serialize(textWriter, cards);
+            }
+        }
+
+        public List<Card> DeserializeFromXml(string filePath)
+        {
+            XmlSerializer XMLDeserializer = new XmlSerializer(typeof(List<Card>));
+            List<Card> cards;
+            using (TextReader textReader = new StreamReader(filePath))
+            {
+                cards = (List<Card>)XMLDeserializer.Deserialize(textReader);
+            }
+            return cards;
+        }
+    }
+}
